eda132c5ec9b6b118aa5cdc8c27835250f841c51
wykhuh <wykhuh@users.noreply.github.com>
2018-09-20 08:00:17 -0700

    display teams and users

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Team < ApplicationRecord
-  has_many :team_assignments
-  has_many :users, through: :team_assignments
+  has_many :users
+
+  def members
+    users.sort_by { |u| u.bio['What role are you interested in?'] }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 Rails.application.routes.draw do
+  resources :teams, only: [:index] do
+    resources :users, only: [:show]
+  end
+
+  root 'teams#index'
 end
